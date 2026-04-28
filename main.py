22980b157bc4963b79639482ce99ac7fbb69a338
srinivas_charlie <94903895+srinivas-charlie@users.noreply.github.com>
2026-04-28 22:21:21 +0530

    login-register pages with db

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, Form, HTTPException
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
-from db import engine
-from models import Base
+from db import engine, get_db
+from models import Base, User
+from sqlalchemy.orm import Session
 
 
 app = FastAPI(debug=True)
@@ -10,9 +12,7 @@ Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def homepage():
-    return ["name", "srinivas"]
+
 
 @app.get("/dashboard")
 def get_dashboard(request: Request):
@@ -23,19 +23,49 @@ def get_dashboard(request: Request):
         
         )
 
-@app.get("/login")
+@app.get("/")
 def login_page(request: Request):
     return templates.TemplateResponse(
     request=request, 
     name="login.html", 
     context={} # Add any other variables here
-)
+
+    )
+
+@app.post("/")
+def login_user(
+    email: str = Form(...),
+    password: str = Form(...),
+    db:Session = Depends(get_db)
+    ):
+
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user or user.password != password:
+       raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    return RedirectResponse(url="/dashboard", status_code=303)
 
 
+@app.get("/register")
+def register_page(request: Request):
+    return templates.TemplateResponse(
+        request=request, 
+        name="register.html", 
+        context={}
+    )
 
+@app.post("/register")
+def create_user(
+    name: str = Form(...), 
+    email: str = Form(...), 
+    password: str = Form(...), 
+    db: Session = Depends(get_db)
+):
+ new_user = User(name=name, email=email, password=password)
 
+ db.add(new_user)
+ db.commit()
+ db.refresh(new_user)
 
-
-
-
-
+ return RedirectResponse(url="/", status_code=303)
