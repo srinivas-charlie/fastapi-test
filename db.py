0334d8c03052c54d8c27144fa7a6e5cb60b34200
srinivas <srinivasshaik19@gmail.com>
2026-05-26 12:26:37 +0530

    containerization

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/postgres"
+DATABASE_URL = "postgresql://postgres:root@db:5432/postgres"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
